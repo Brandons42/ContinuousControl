2cf231cebbd4474031e94251da6008a9ac77d3b6
Nikhil Masinete <nikhilmasinete@gmail.com>
2020-04-17 07:52:04 +0200

    Completed

--- Agent.py
+++ Agent.py
@@ -103,3 +103,19 @@ class OUNoise:
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
+    
+    def add(self, state, action, reward, next_state, done):
+        e = self.experience(state, action, reward, next_stae, done)
+        self.memory.append(e)
+        
+    def sample(self):
+        experiences = random.sample(self.memory, k=self.batch_size)
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None])).float().to(device)
+        
+        return(states, actions, rewards, next_states, dones)
+    def __len__(self):
+        return len(self.memory)
